0386b1514ad46021b2a0740bf9d15a929b16634b
jorgetelles <jorgewtelles123@gmail.com>
2024-11-13 18:42:46 -0300

    Add files via upload

---
+++ aula_01_01.py
@@ -0,0 +1,6 @@
+n1 = 10
+n2 = 5.6
+nome =  "jorge "
+idade = 50
+print("sr(a) ", nome " , a sua idade é" , idade)
+ 
---
+++ aula_01_02.py
@@ -0,0 +1,3 @@
+num =  int(input('informe um valor : ')) 
+result = num * 2 
+print(' o resultado doa dobro do númeor é : ' , result) 
